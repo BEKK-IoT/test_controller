bb9c28a3991a9ed6702dd890a8c9ba65059bd083
alexander-svendsen <alexander.svendsen@bekk.no>
2015-09-20 18:25:50 +0200

    Add a simple controller

---
+++ app.js
@@ -0,0 +1,33 @@
+import keypress from 'keypress';
+import {firebase} from 'devices-core';
+
+let fire = new firebase("Alex", 'gadgets/game/')
+keypress(process.stdin);
+
+// listen for the "keypress" event
+process.stdin.on('keypress', function (ch, key) {
+  console.log('got "keypress"', key);
+
+  if (key && key.ctrl && key.name == 'c') {
+      process.stdin.pause();
+      process.exit();
+  }
+
+  if (key) {
+    if (key.name == "left"){
+      console.log("left");
+      fire.send("move", "left");
+    }
+    else if (key.name == "right"){
+      console.log("right");
+      fire.send("move", "right");
+    }
+    else if (key.name == "up"){
+      console.log("up");
+      fire.send("move", "up");
+    }
+  }
+});
+
+process.stdin.setRawMode(true);
+process.stdin.resume();
